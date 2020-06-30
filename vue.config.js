b7b3c61758523e7085deb9e1974a53da0af844c3
shaozhilin <502121489@qq.com>
2020-06-30 20:57:44 +0800

    lint-staged

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,23 @@
 'use strict'
-const path = require('path')
-const idcMain = require('./src/idc-sdk/idc.main.js')
+// const path = require('path')
 
-function resolve(dir) {
-  return path.join(__dirname, dir)
-}
+// function resolve (dir) {
+//   return path.join(__dirname, dir)
+// }
 
-const port = process.env.port || process.env.npm_config_port || 9527 // 端口
+// const port = process.env.port || process.env.npm_config_port // 端口
 
-module.exports={
-    publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
+module.exports = {
+  // 默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上
+  // 例如 https://localhost/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://localhost/admin/，则设置 baseUrl 为 /admin/。
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  outputDir: 'dist',
+  // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
+  assetsDir: 'static',
+  // 是否开启eslint保存检测，有效值：ture | false | 'error'
+  lintOnSave: process.env.NODE_ENV === 'development',
+  // 打包时不生成.map文件 避免看到源码
+  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+  productionSourceMap: false
+  // webpack-dev-server 相关配置
 }
