dd223a55bb026a7e9e9797d5819deca9fa816c2e
shaozhilin <502121489@qq.com>
2020-06-30 14:11:09 +0800

    添加echarts依赖

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import './plugins/axios'
 import './plugins/element.js'
+import 'animate.css'
 Vue.config.productionTip = false
 
 new Vue({
